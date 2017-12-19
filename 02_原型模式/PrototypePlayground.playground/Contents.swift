76596cc38ef9490fe0d6e33a56b87c97a6d8a7c4
Rocky <aoyun@zhongchebaolian.com>
2017-12-19 17:49:10 +0800

    add  prototype playground

---
+++ 02_原型模式/PrototypePlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+///结构体为值类型，默认会就行深拷贝
+struct Appointment{
+    var name:String
+    var day:String
+    var place:String
+
+    func printDetail(label:String) {
+        print("\(label) with \(name) on \(day) at \(place)")
+    }
+    
+}
+
+var  beerMeeting = Appointment(name: "Bob", day: "Mon", place: "BeiJing")
+beerMeeting.printDetail(label: "beerMeeting")
+
+var workMeeting = beerMeeting
+workMeeting.name = "Alice"
+workMeeting.day = "Fri"
+//workMeeting.place = "Shanghai"
+
+workMeeting.printDetail(label: "workmeeting")
+
+let beerPointer = withUnsafePointer(to: &beerMeeting, { $0 })
+let workPointer = withUnsafePointer(to: &workMeeting, { $0 })
+
+print(beerPointer)
+print(workPointer)
+
+print(beerMeeting)
+print(workMeeting)
+
+
+
