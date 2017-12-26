0363cac5c2dc5da0e3030c5556e590daabf0ce0c
Rocky <aoyun@zhongchebaolian.com>
2017-12-26 17:34:12 +0800

    SportsStore 实现简单工厂

--- 05_工厂模式/SportsStore/SportsStore/Product.swift
+++ 05_工厂模式/SportsStore/SportsStore/Product.swift
@@ -8,17 +8,43 @@
 
 import Foundation
 
+enum UpsellOpportunities {
+    case SwimmingLessons
+    case MapOfLakes
+    case SoccerVideos
+}
+
 class Product:NSCopying {
     
     
+    class func createProduct(_ name:String,
+                             description:String,
+                             category:String,
+                             price:Double,
+                             stockLevel:Int) -> Product{
+        
+        var productType:Product.Type
+        
+        switch category {
+        case "Watersports":
+            productType = WatersportsProduct.self
+        case "Soccer":
+            productType = SoccerProduct.self
+        default:
+            productType = Product.self
+        }
+        
+        return productType.init(name: name, description: description, category: category, price: price, stockLevel: stockLevel)
+    }
     
     private(set) var name:String
     private(set) var description:String
     private(set) var category:String
     private var stockLevelBackingValue:Int = 0
     private var priceBackingValue:Double = 0
+    fileprivate var salesTaxRate:Double = 0.2
     
-    init(name:String,description:String,category:String,price:Double,stockLevel:Int) {
+    required init(name:String,description:String,category:String,price:Double,stockLevel:Int) {
         self.name = name
         self.description = description
         self.category = category
@@ -38,12 +64,42 @@ class Product:NSCopying {
     }
     
     var stockValue:Double{
-        get { return price * Double(stockLevel) }
+        get { return price * (1 + salesTaxRate) * Double(stockLevel) }
     }
+    
+    var upsells:[UpsellOpportunities]{
+        get{
+            return Array()
+        }
+    }
+    
     
     func copy(with zone: NSZone? = nil) -> Any {
         return Product(name: self.name,
                        description: self.description,
                        category: self.category, price: self.price, stockLevel: self.stockLevel)
+    }
+}
+
+
+class WatersportsProduct: Product {
+    required init(name:String,description:String,category:String,price:Double,stockLevel:Int) {
+        super.init(name: name, description: description, category: category, price: price, stockLevel: stockLevel)
+        self.salesTaxRate = 0.1
+    }
+    
+    override var upsells: [UpsellOpportunities]{
+        return [UpsellOpportunities.SwimmingLessons,UpsellOpportunities.MapOfLakes]
+    }
+}
+
+class SoccerProduct: Product {
+    required init(name:String,description:String,category:String,price:Double,stockLevel:Int) {
+        super.init(name: name, description: description, category: category, price: price, stockLevel: stockLevel)
+        self.salesTaxRate = 0.25
+    }
+    
+    override var upsells: [UpsellOpportunities]{
+        return [UpsellOpportunities.SoccerVideos]
     }
 }
