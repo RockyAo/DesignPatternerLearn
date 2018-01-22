d14128fb831ba428dd57e4ee661ca52830404a79
Rocky <aoyun@zhongchebaolian.com>
2018-01-22 19:16:46 +0800

    实现装饰器模式

--- 09_装饰器模式/Decorator/Decorator/main.swift
+++ 09_装饰器模式/Decorator/Decorator/main.swift
@@ -12,5 +12,11 @@ let account = CustomerAccount(name: "Joe")
 
 account.addPurchase(Purchase(product: "Red Hat", price: 10.0))
 account.addPurchase(Purchase(product: "Scarf", price: 20))
-
+account.addPurchase(PurchaseWithDelivery(
+    purchase: PurchaseWithGiftWrap(
+        purchase: Purchase(product: "Sunglasses",
+                           price: 25)
+        )
+    )
+)
 account.printAccount()
