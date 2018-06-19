edf44b012d5ac7d14870d880121358763553e2f7
Rocky <542622608@qq.com>
2018-06-19 21:43:05 +0800

    FactoryPattern

---
+++ 21_anotherDemo/CoffeeQuest/CoffeeQuest/Factories/AnnotationFactory.swift
@@ -0,0 +1,44 @@
+//
+//  AnnotationFactory.swift
+//  CoffeeQuest
+//
+//  Created by Rocky on 2018/6/19.
+//  Copyright © 2018年 Jay Strawn. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import UIKit
+import YelpAPI
+
+public class AnnotationFactory {
+  public func createBusinessMapViewModel(for business:YLPBusiness) -> BusinessMapViewModel? {
+    guard let yelpCoordinate = business.location.coordinate else {
+      return nil
+    }
+    
+    let coordinate = CLLocationCoordinate2D(latitude: yelpCoordinate.latitude,
+                                            longitude: yelpCoordinate.longitude)
+    let name = business.name
+    let rating = business.rating
+    let image: UIImage
+    
+    switch rating {
+    case 0.0..<3.5:
+      image = UIImage(named: "bad")!
+    case 3.5..<4.0:
+      image = UIImage(named: "meh")!
+    case 4.0..<4.75:
+      image = UIImage(named: "good")!
+    case 4.75..<5.0:
+      image = UIImage(named: "great")!
+    default:
+      image = UIImage(named: "bad")!
+    }
+    
+    return BusinessMapViewModel(coordinate: coordinate,
+                                          name: name,
+                                          rating: rating,
+                                          image: image)
+  }
+}
